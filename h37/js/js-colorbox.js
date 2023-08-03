bbb1a20fe97a6decc6b9061fa89c3ddee3a620d9
Hank de Wit <hdewit.au@gmail.com>
2023-08-04 00:25:31 +0930

    Creating h37 tree
    H37 mitochondrial tree

---
+++ h37/js/js-colorbox.js
@@ -0,0 +1,15 @@
+			$(document).ready(function(){
+				$(".info").colorbox({iframe:true, width:"780px", height:"80%", title:false, opacity:0.25});
+			});
+			$(document).ready(function(){
+				$(".info2").colorbox({iframe:true, width:"70%", height:"80%", title:false, opacity:0.25});
+			});
+			$(document).ready(function(){
+				$(".info3").colorbox({iframe:true, width:"95%", height:"95%", title:false, opacity:0.25});
+			});
+                        $(document).ready(function(){
+				$(".info4").colorbox({iframe:true, width:"300px", height:"30%", title:false, opacity:0.25});
+			});
+ $(document).ready(function(){
+				$(".info5").colorbox({iframe:true, width:"800px", height:"520px", title:false, opacity:0.25});
+			});
